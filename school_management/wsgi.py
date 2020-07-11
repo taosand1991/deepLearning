d95dab5487e490fe5844ad3bf4f15e4d7c064cfb
TAOSAND <taofeekdboy@yahoo.com>
2020-07-11 19:51:58 +0400

    make it better

--- school_management/wsgi.py
+++ school_management/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_management.settings')
+project_folder = os.path.expanduser('~/school_management')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
+
+
